c3c800d0ef083b70f605913eaf96587ea813e5f2
Henry930127 <henry60109@gmail.com>
2024-05-10 21:37:37 +0800

    model complete
    GPS, MaintenanceRecord, RentalRecord

--- src/main/java/com/escooter/api/model/RentalRecord.java
+++ src/main/java/com/escooter/api/model/RentalRecord.java
@@ -6,4 +6,41 @@ public class RentalRecord {
     private LocalTime startTime;
     private LocalTime endTime;
     private boolean isPaid;
+
+    public String getUserAccount(){
+        return userAccount;
+    }
+    public String getEscooterId(){
+        return escooterId;
+    }
+    public LocalTime getStartTime(){
+        return startTime;
+    }
+    public LocalTime getEndTime(){
+        return endTime;
+    }
+    public boolean getIsPaid(){
+        return isPaid;
+    }
+
+    public void setUserAccount(String userAccount)
+    {
+        this.userAccount = userAccount;
+    }
+    public void setEscooterId(String escooterId)
+    {
+        this.escooterId = escooterId;
+    }
+    public void setStartTime(LocalTime startTime)
+    {
+        this.startTime = startTime;
+    }
+    public void setEndTime(LocalTime endTime)
+    {
+        this.endTime = endTime;
+    }
+    public void setIsPaid(boolean isPaid)
+    {
+        this.isPaid = isPaid;
+    }
 }
