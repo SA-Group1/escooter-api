477081a98e818b1f01c401d6fd1344ef258e9156
Henry930127 <henry60109@gmail.com>
2024-05-11 21:12:19 +0800

    RentalHistoryService

--- src/main/java/com/escooter/api/controller/EscooterController.java
+++ src/main/java/com/escooter/api/controller/EscooterController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.escooter.api.model.*;
 import com.escooter.api.service.*;
-//import com.escooter.api.service.*;
-//import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
@@ -18,6 +16,7 @@ public class EscooterController {
     MaintenanceService maintenanceService;
     RentalService rentalService;
 
+    //未完成
     @RequestMapping("/addEscooter")
     public String hello(@RequestParam String param) {
         return "Hello" ;
--- src/main/java/com/escooter/api/service/RentalHistoryService.java
+++ src/main/java/com/escooter/api/service/RentalHistoryService.java
@@ -1,5 +1,15 @@
 package com.escooter.api.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import com.escooter.api.model.*;
+@Service
 public class RentalHistoryService {
+    
+    /*public List<RentalRecord> serchHistory(User user){
+        return ;
+    }*/
     
 }
