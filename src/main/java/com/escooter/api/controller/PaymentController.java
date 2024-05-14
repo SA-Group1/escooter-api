c9f43bdecb60f98a28629b025c0baebac91d64c0
Uselessfishh <114258543+Uselessfishh@users.noreply.github.com>
2024-05-14 10:34:18 +0800

    refactor: Move credit card functionality to payment service.

--- src/main/java/com/escooter/api/controller/PaymentController.java
+++ src/main/java/com/escooter/api/controller/PaymentController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.boot.configurationprocessor.json.JSONException;
 import org.springframework.boot.configurationprocessor.json.JSONObject;
 
+import com.escooter.api.dto.CreditCardDTO;
+import com.escooter.api.model.CreditCard;
 import com.escooter.api.dto.MemberCardDTO;
 import com.escooter.api.model.MemberCard;
 import com.escooter.api.service.PaymentService;
@@ -25,18 +27,43 @@ public class PaymentController {
     private PaymentService paymentService;
 
 	/**
+	 * Adding credit card data to database and returns a successful message
+	 *
+	 * @param creditCardDTO Credit card data include cvv
+	 * @param cvv Credit card cvv number
+	 * @return A ResponseEntity with http status and message
+	 */
+	 @PostMapping("/bindCreditCard")
+	 public ResponseEntity<String> addCreditCard(@RequestBody CreditCardDTO creditCardDTO){
+ 
+		 // call service to add credit card
+		 CreditCard creditCard = new CreditCard(creditCardDTO.getCardNumber(), creditCardDTO.getExpirationDate(), creditCardDTO.getCardHolderName());
+		 paymentService.bindCreditCard(creditCard,creditCardDTO.getCvv());
+ 
+		 // create return message
+		 JSONObject message = new JSONObject();
+		 try {
+			 message.put("status", true);
+			 message.put("message", "Adding credit card success");
+		 } catch (JSONException e) {
+			 e.printStackTrace();
+		 }
+ 
+		 return new ResponseEntity<>(message.toString(), HttpStatus.OK);
+	 }
+
+	/**
 	 * Adding member card data to database and returns a successful message
 	 *
 	 * @param memberCardDTO Member card data
 	 * @return A ResponseEntity with http status and message
 	 */
-
 	@PostMapping("/bindMemberCard")
 	public ResponseEntity<String> bindMemberCard(@RequestBody MemberCardDTO memberCardDTO){
 
 		// call service to add member card
 		MemberCard memberCard = new MemberCard(memberCardDTO.getCardNumber(),memberCardDTO.getExpirationDate());
-        paymentService.addCard(memberCard);
+        paymentService.bindMemberCard(memberCard);
 
 		// create return message
 		JSONObject message = new JSONObject();
--- src/main/java/com/escooter/api/service/PaymentService.java
+++ src/main/java/com/escooter/api/service/PaymentService.java
@@ -3,6 +3,8 @@ package com.escooter.api.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.escooter.api.model.CreditCard;
+import com.escooter.api.repository.CreditCardRepository;
 import com.escooter.api.model.MemberCard;
 import com.escooter.api.repository.MemberCardRepository;
 
@@ -13,14 +15,32 @@ import com.escooter.api.repository.MemberCardRepository;
 public class PaymentService {
 
 	@Autowired
+	private CreditCardRepository creditCardRepository;
+	@Autowired
 	private MemberCardRepository memberCardRepository;
+
+	/**
+     * Adds a new credit card if it is valid.
+     * @param creditCard The credit card to add.
+     * @param cvv The CVV (Card Verification Value) of the credit card.
+     * @return True if the credit card is valid and added successfully, false otherwise.
+     */
+	public boolean bindCreditCard(CreditCard creditCard , String cvv){
+
+		if(!creditCard.isVaild(cvv)){
+			return false;
+		}
+
+		creditCardRepository.addCard(creditCard);
+		return true;
+	}
 
 	/**
      * Adds a new member card if it is valid.
      * @param memberCard The member card to add.
      * @return True if the member card is valid and added successfully, false otherwise.
      */
-	public boolean addCard(MemberCard memberCard){
+	public boolean bindMemberCard(MemberCard memberCard){
 
 		if(!memberCard.isVaild()){
 			return false;
